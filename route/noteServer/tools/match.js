fded7fa14a4693814043f5c3abb9733314dac6c9
GaoLiHai <1019933576@qq.com>
2020-08-11 18:12:03 +0800

    add annotation

--- route/noteServer/tools/match.js
+++ route/noteServer/tools/match.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-// 获取文件列表
+// 递归获取文件列表
 function getDir(dir, result) {
     result = result || [];
     let fileList = fs.readdirSync(dir);
@@ -15,29 +15,45 @@ function getDir(dir, result) {
 
     return result;
 }
+
+// 匹配 key，返回预定的接口
+// interface SearchResult {
+//     title: string
+//     link: string
+//     label: string?
+//     description: string?
+//   }
 module.exports = async function match(key) {
+    // 调 getDir 拿到文件列表
     let fileList = getDir('route/noteServer/public/doc', []);
+    // 正则
     let regexp = new RegExp(`.{0,}${key}.{0,}`, 'i');
+    // 储存返回的结果
     let result = []
 
+    // 遍历文件列表，同步读文件匹配
     for (file of fileList) {
-
         let find = regexp.exec(await fs.readFileSync(file));
         if (find) {
+            // 取到文件名最后三个部分
             let middleTemp = file && file.replace(/route\/noteServer\/public\//g, '').split('/');
+
             if (middleTemp && middleTemp.length == 3) {
+                // 一级目录下的 md 处理
                 var filenameObj = {
                     father: middleTemp[1],
                     child: middleTemp[2],
                     link: '/' + middleTemp[0] + '/' + middleTemp[1] + '/' + middleTemp[2]
                 }
             } else {
+                // 根目录的 md 处理
                 var filenameObj = {
                     father: null,
                     child: file && '/' + file.replace(/route\/noteServer\/public\/doc\//g, ''),
                     link: file.replace(/route\/noteServer\/public\//g, ''),
                 }
             }
+            // 最多返回 16 条
             if (result.length >= 16) {
                 return result;
             }
