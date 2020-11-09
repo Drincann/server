cfe867c43c53459ccbbfce4508063c05a4379c9e
GaoLiHai <1019933576@qq.com>
2020-11-09 21:01:44 +0800

    修复了月份

--- route/submit/tools/getTodayStr.js
+++ route/submit/tools/getTodayStr.js
@@ -3,6 +3,6 @@ module.exports = function getTodayStr() {
         return number <= 9 ? '0' + number : number;
     }
     let today = new Date();
-    return '' + today.getFullYear() + getDouble(today.getMonth()) +
+    return '' + today.getFullYear() + getDouble(today.getMonth() + 1) +
         getDouble(today.getDate());
 }
