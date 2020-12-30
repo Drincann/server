87110fdf41a2a23809698370246cce46f81ff1e5
GaoLiHai <1019933576@qq.com>
2020-12-30 14:18:56 +0800

    取消 https 重定向

--- middleware/httpsRedirect.js
+++ middleware/httpsRedirect.js
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
-    if (process.env.NODE_ENV == 'development') {
-    } else {
-        if (!req.secure) {
-            return res.redirect('https://' + req.get('host') + req.url);
-        }
-    }
+    // if (process.env.NODE_ENV == 'development') {
+    // } else {
+    //     if (!req.secure) {
+    //         return res.redirect('https://' + req.get('host') + req.url);
+    //     }
+    // }
     next();
 }
